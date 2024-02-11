deb7a0b05cf4514abf043a02e5723217233763cd
Ronny011-dy <ronny.paz@dynamicyield.com>
2024-02-11 22:49:10 +0200

    added all crud routes

--- server/api/routes.ts
+++ server/api/routes.ts
@@ -22,52 +22,47 @@ export const router = new Router();
 const prisma = new PrismaClient();
 
 const main = async () => {
-  router.get("/", async (ctx) => {
-    ctx.body = "Hello world";
-  });
-
   router.get("/api/get-all", async (ctx) => {
     const allPlaces: Place[] = await prisma.place.findMany();
     ctx.body = allPlaces;
   });
-};
 
-router.post("/api/insert", async (ctx) => {
-  const {
-    id,
-    rest_name,
-    rest_image,
-    rest_url,
-    rate_food,
-    rate_service,
-    rate_vibe,
-    rate_final,
-    notes,
-    city,
-    cuisine,
-    keywords,
-    did_visit,
-  } = ctx.request.body as Place;
-  const newPlace = await prisma.place.create({
-    data: {
-      id,
-      rest_name,
-      rest_image,
-      rest_url,
-      rate_food,
-      rate_service,
-      rate_vibe,
-      rate_final,
-      notes,
-      city,
-      cuisine,
-      keywords,
-      did_visit,
-    },
+  router.get("/api/get:id", async (ctx) => {
+    const id = Number(ctx.params.id);
+    const foundPlace = await prisma.place.findUniqueOrThrow({
+      where: { id },
+    });
+    ctx.status = foundPlace ? 201 : 400;
+    ctx.body = foundPlace;
   });
-  ctx.status = 201;
-  ctx.body = newPlace;
-});
+
+  router.post("/api/insert", async (ctx) => {
+    const data = ctx.request.body as Place;
+    const newPlace = await prisma.place.create({ data });
+    ctx.status = 201;
+    ctx.body = newPlace;
+  });
+
+  router.post("/api/update:id", async (ctx) => {
+    const id = Number(ctx.params.id);
+    const data = ctx.request.body as Partial<Place>;
+    const updatedPlace = await prisma.place.update({
+      where: { id },
+      data,
+    });
+    ctx.status = 201;
+    ctx.body = updatedPlace;
+  });
+
+  router.post("/api/delete:id", async (ctx) => {
+    const id = Number(ctx.params.id);
+    const deletedPlace = await prisma.place.delete({
+      where: { id },
+    });
+    ctx.status = 201;
+    ctx.body = deletedPlace;
+  });
+};
 
 main()
   .then(async () => {
